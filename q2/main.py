04c62d0a3bfeda89002b7b9da8ea9dc1a4ca58cd
Hyan Batista <hyanbatista42@gmail.com>
2022-08-21 21:36:01 -0300

    Add second question (wip)

--- q2/main.py
+++ q2/main.py
@@ -1,100 +1,92 @@
 from __future__ import annotations
 
-from queue import Queue
-from uuid import uuid4
+import random
+from dataclasses import dataclass
+from uuid import UUID, uuid4
 
 
-class Page:
-    def __init__(self, size):
-        self.size: int = size
-
-
+@dataclass
 class Process:
-    def __init__(self, id: int, name: str, size: int, page_size: int) -> None:
-        self.id: int = id
-        self.name: str = name
-        self.size: int = size
-        self.page_size: int = page_size
-        self.number_of_pages: int = 0
-        self.max_number_of_pages: int = self._get_max_number_of_pages()
-        self.pages: Queue = Queue()
+    id: int
+    name: str
+    size: int
 
-    def allocate(self):
-        page = Page(self.page_size)
-        if self.number_of_pages + 1 <= self.max_number_of_pages:
-            self.pages.put(page)
-            self.number_of_pages += 1
-        else:
-            self._substitute(page)
-    
-    def _substitute(self, page: Page):
-        dumped = self.pages.get()
-        self.pages.put(page)
-        return dumped
+def create_process(id: int, size: int):
+    return Process(id, f"Process {id}", size)
 
-    def _get_max_number_of_pages(self):
-        return self.size // self.page_size
+
+@dataclass
+class Page:
+    def __init__(self, size: int, process: Process):
+        self.size = size
+        self.process = process
+        self.id = uuid4()
 
 
 class Memory:
     def __init__(self, max_size: int):
-        self.size: int = 0
         self.max_size: int = max_size
-        self.processes: Queue = Queue()
+        self.pages: list[Page] = []
+        self.current_size: int = 0
 
-    def allocate(self, process: Process) -> Process | None:
-        if self._predicted_memory_size(process) <= self.max_size:
-            self.size += process.size
-            self.processes.put(process)
-        else:
-            raise Exception("Max memory size exceeded.")
 
-    def _predicted_memory_size(self, process: Process):
-        return self.size + process.size
+class MemoryError(Exception):
+    pass
+
+
+def move_page_to_memory(memory: Memory, page: Page):
+    if (memory.current_size + page.size) <= memory.max_size:
+        memory.pages.append(page)
+        memory.current_size += page.size
+    else:
+        raise MemoryError("Max memory size exceeded.")
+
+
+def remove_page_from_memory(memory: Memory, page: Page):
+    memory.pages.remove(page)
+    memory.current_size -= page.size
 
 
 class VirtualMemory:
-    def __init__(self, max_size: int):
-        self.max_size: int = max_size
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.pages: list[Page] = []
+        self.current_size: int = 0
 
 
-class PageReferenceService:
-    def __init__(self):
-        self.pages: Queue = Queue()
-    
-    def put(self, page: Page):
-        self.pages.put(page)
-
-    def get(self):
-        page = self.pages.get()
-        self.pages.put(page)
-        return page
-
-
-def create_process(size: int, page_size: int):
-    id = uuid4()
-    process = Process(id, f"Process {id}", size, page_size)
-    for _ in process.max_number_of_pages():
-        process.allocate(Page(page_size))
-    return process
-
-
-def apply_fifo_substitution(process: Process) -> Process:
-    return process
-
-
-def apply_lru_substitution(process: Process) -> Process:
-    return process
-
-
-def run_substitution_algorithm(process: Process):
-    print("Select substitution algorithm. Enter (1) for FIFO and (2) LRU:")
-    alternative = int(input())
-
-    if alternative == 1:
-        apply_fifo_substitution(process)
+def move_page_to_virtual_memory(virtual_memory: Memory, page: Page):
+    if virtual_memory.current_size + page.size <= virtual_memory.max_size:
+        virtual_memory.pages.append(page)
+        virtual_memory.current_size += page.size
     else:
-        apply_lru_substitution(process)
+        raise Exception("Max virtual memory size exceeded.")
+
+
+def remove_page_from_virtual_memory(virtual_memory: Memory, page: Page):
+    virtual_memory.pages.remove(page)
+    virtual_memory.current_size -= page.size
+
+
+def create_process_pages(process: Process, page_size: int):
+    pages: list[Page] = []
+    number_of_pages = process.size // page_size
+    for _ in range(number_of_pages):
+        pages.append(Page(page_size, process))
+    return pages
+
+
+def apply_fifo_substitution(memory: Memory, virtual_memory: VirtualMemory, page: Page) -> Page:
+    popped = memory.pages.pop(0)
+    memory.current_size -= popped.size
+    move_page_to_virtual_memory(virtual_memory, popped)
+    move_page_to_memory(memory, page)
+
+
+def apply_lru_substitution(memory: Memory, virtual_memory: VirtualMemory, page: Page) -> Page:
+    popped = memory.pages[0]
+    memory.current_size -= popped.size
+    move_page_to_virtual_memory(virtual_memory, popped)
+    move_page_to_memory(memory, page)
 
 
 def initial_setup():
@@ -104,40 +96,62 @@ def initial_setup():
     max_virtual_memory_size = int(input())
     print("Enter page size (MB):")
     page_size = int(input())
-    return max_memory_size, max_virtual_memory_size, page_size
+    print("Enter the substitution algorithm. (1) for FIFO and (2) for LRU:")
+    algorithm = int(input())
+    return max_memory_size, max_virtual_memory_size, page_size, algorithm
 
-def print_memory_allocation_info(memory: Memory):
+
+def create_processes_from_file(path: str) -> list[Process]:
+    processes = []
+    with open(path) as f:
+        for line in f.readlines():
+            id, size = line.split()
+            processes.append(create_process(int(id), int(size)))
+    return processes
+
+
+def print_memory_allocation_info(memory: Memory, virtual_memory: VirtualMemory):
     print("----------------------------------------------------")
     print("MEMORY ALLOCATION")
-    print(f"Memory State/Capacity: {memory.size}/{memory.max_size}")
-    print(f"Is Memory Fragmented?: #TODO")
-    print(f"Pages Free/Allocated: {2}/{2}")
+    print(f"Memory State/Capacity: {memory.current_size}/{memory.max_size}")
+    print(f"Virtual Memory State/Capacity: {virtual_memory.current_size}/{virtual_memory.max_size}")
+    fragmented = memory.current_size == memory.max_size
+    print(f"Is Memory Fragmented?: {fragmented}")
+    print(f"Pages Free/Allocated: {len(memory.pages)}/{len(virtual_memory.pages)}")
+
 
 def main():
-    max_memory_size, max_virtual_memory_size, page_size = initial_setup()
-    stop = False
+    path = "q2/in.txt"
+    max_memory_size, max_virtual_memory_size, page_size, algorithm = initial_setup()
     memory = Memory(max_memory_size)
     virtual_memory = VirtualMemory(max_virtual_memory_size)
-    page_reference_service = PageReferenceService()
-    free_pages = 0
-    allocated_pages = 0
+    page_references = []
+    page_miss = 0
 
-    while not stop:
-        print("Enter the process size (MB):")
-        size = int(input())
-        process = create_process(size, page_size)
+    processes = create_processes_from_file(path)
 
+    for process in processes:
+        page_references.extend(create_process_pages(process, page_size))
+
+    print(f"Number of pages: {len(page_references)}")
+    for _ in range(50):
+        index = random.randint(0, len(page_references) - 1)
+        page: Page = page_references[index]
+        print(f"Page index: {index}")
+
+        try:
+            if page.id not in [page.id for page in memory.pages]:
+                move_page_to_memory(memory, page)
+                page_miss += 1
+        except MemoryError:
+            if algorithm == 1:
+                apply_fifo_substitution(memory, virtual_memory, page)
+            else:
+                apply_lru_substitution(memory, virtual_memory, page)
         
-        
-        # try:
-        #     pass
-        # except:
-        #     run_substitution_algorithm(process)
-        
-        print_memory_allocation_info(memory)
+        print_memory_allocation_info(memory, virtual_memory)
 
-
-    print_memory_allocation_info(memory)
+    print(f"Page misses: {page_miss}")
 
 
 if __name__ == "__main__":
